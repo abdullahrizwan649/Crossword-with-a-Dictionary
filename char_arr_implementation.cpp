1817d76118e448c3f806ef6ae04778d135245987
Abdullah Rizwan <100567651+BOYYY743@users.noreply.github.com>
2022-11-10 15:19:22 +0500

    original character array inserted

--- char_arr_implementation.cpp
+++ char_arr_implementation.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+#define row 7
+#define column 12
 
 
 
@@ -9,35 +11,75 @@ using namespace std;
 int main()
 {
 
-    int row_num, column_num;
-    cout<<"Enter the number of rows of 2D array ";
-    cin>>row_num;
-    cout<<"Enter the number of columns of 2D array ";
-    cin>>column_num;
+    char arr[row][column] = {
+                             {'d','o','g','x','b','x','x','x','n','o','o','n'}
+                            ,{'r','h','e','l','l','o','t','h','e','r','e','x'}
+                            ,{'o','k','c','i','u','q','b','r','o','w','n','m'}
+                            ,{'w','x','w','g','e','x','l','x','h','j','i','j'}
+                            ,{'o','o','z','o','k','v','u','x','d','r','o','w'}
+                            ,{'r','l','x','d','r','x','e','x','t','x','j','a'}
+                            ,{'d','r','o','w','b','l','o','n','k','g','o','d'}
+                            };
 
-    char* arr = new char[row_num * column_num];
-    int c = 'a';
-    // Traverse the 2D array
-    for (int i = 0; i < row_num; i++) {
-        for (int j = 0; j < column_num; j++) {
- 
-            // Assign values to
-            // the memory block
-            *(arr + i * column_num + j) = ++c;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cout<<arr[i][j]<<" ";
         }
+        cout<<endl;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // int row_num, column_num;
+    // cout<<"Enter the number of rows of 2D array ";
+    // cin>>row_num;
+    // cout<<"Enter the number of columns of 2D array ";
+    // cin>>column_num;
+
+    // char* arr = new char[row_num * column_num];
+    // int c = 'a';
+    // // Traverse the 2D array
+    // for (int i = 0; i < row_num; i++) {
+    //     for (int j = 0; j < column_num; j++) {
  
-    // Traverse the 2D array
-    for (int i = 0; i < row_num; i++) {
-        for (int j = 0; j < column_num; j++) {
+    //         // Assign values to
+    //         // the memory block
+    //         *(arr + i * column_num + j) = ++c;
+    //     }
+    // }
  
-            // Print values of the
-            // memory block
-            cout << *(arr + i * column_num + j)
-                 << " ";
-        }
-        cout << endl;
-    }
+    // // Traverse the 2D array
+    // for (int i = 0; i < row_num; i++) {
+    //     for (int j = 0; j < column_num; j++) {
+ 
+    //         // Print values of the
+    //         // memory block
+    //         cout << *(arr + i * column_num + j)
+    //              << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     
     return 0;
